ecac9e355d1e7a0903cd2884e6e5753798c4c675
Milo Goodfellow <milogoodfellowworkemail@gmail.com>
2021-12-25 21:48:13 -0500

    New search method
    New search method

--- single_page/static/single_page/javascript/single_page.js
+++ single_page/static/single_page/javascript/single_page.js
@@ -40,19 +40,17 @@ $(document).ready(function(){
 
   $("#search-input-large").on("change enter", function(){
     console.log('Test');
-    search_params = new URLSearchParams(window.location.href);
-
-    search_params.set('search',document.getElementById("search-input-large").innerHTML.split(' ').join('_'));
-    window.location.replace(window.location.href + '/?'+search_params.toString());
+    new_url = new URL(window.location.origin);
+    url.append('search',document.getElementById("search-input-large").value.split(' ').join('_'));
+    window.location.replace(new_url.href);
 
   })
 
   $("#search-input-small").on("change enter", function(){
     console.log('Test');
-    search_params = new URLSearchParams(window.location.href);
-
-    search_params.set('search',document.getElementById("search-input-small").innerHTML.split(' ').join('_'));
-    window.location.replace(window.location.href + '/?'+search_params.toString());
+    new_url = new URL(window.location.origin);
+    url.append('search',document.getElementById("search-input-small").value.split(' ').join('_'));
+    window.location.replace(new_url.href);
 
   })
 
